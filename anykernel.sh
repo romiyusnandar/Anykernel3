8ea1a095a164f784a52193e859ee0783e92a9471
Romiyus <104280247+Anothermi1@users.noreply.github.com>
2023-01-17 07:56:47 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Cakeby Kernel  by romiyusnandar @telegram
+kernel.string=Cakeby Kernel  by @romiyusnandar @telegram
 do.devicecheck=1
 do.modules=0
 do.systemless=1
