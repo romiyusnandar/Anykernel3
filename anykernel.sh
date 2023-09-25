48beb964438a22f8be47237a873a8673fd44e4d2
Romiyus <104280247+Yusna21@users.noreply.github.com>
2023-09-26 04:37:20 +0700

    Update anykernel.sh
    update anykernel for topaz/tapas

--- anykernel.sh
+++ anykernel.sh
@@ -4,41 +4,49 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string= 
-maintainer.string= Kernel by Romi Yusnandar
+kernel.string=Quantum Charge by yusna21
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=tissot
-supported.versions=9 - 13.1
-supported.patchlevels=
+device.name1=topaz
+device.name2=tapas
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
 '; } # end properties
 
-
 # shell variables
-block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
+slot=$(find_slot);
+block=/dev/block/bootdevice/by-name/boot$slot;
 is_slot_device=1;
 ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+## AnyKernel install
+split_boot;
 
-## AnyKernel boot install
-dump_boot;
-# begin ramdisk changes
+## Selinux Permissive
+#patch_cmdline androidboot.selinux androidboot.selinux=permissive
 
-write_boot;
-## end boot install
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
 
-ui_print "kernel installed successfully"
+# end ramdisk changes
+
+flash_boot;
+## end install
