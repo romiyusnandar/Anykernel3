d3d8696224d32d7b602f74bf610daca32685905d
sirNewbies <104280247+sirnewbies@users.noreply.github.com>
2023-10-03 06:12:53 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Quantum Charge by yusna21
+kernel.string=Quantum Charge
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -50,3 +50,6 @@ fi;
 
 flash_boot;
 ## end install
+
+sleep 3
+ui_print "kernel installed successfully"
