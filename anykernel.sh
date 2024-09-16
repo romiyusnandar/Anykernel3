32776e64cd213aaf7506ae86c194678cb06aeaa4
Romi Yusnandar <104280247+romiyusnandar@users.noreply.github.com>
2024-09-16 19:16:49 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -10,16 +10,20 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=topaz
-device.name2=tapas
-device.name3=xun
-supported.versions=14 - 14.1
+device.name1=sweet
+device.name2=sweetin
+device.name3=
+supported.versions=11 - 14.1
 '; } # end properties
 
-# shell variables
-slot=$(find_slot);
-block=/dev/block/bootdevice/by-name/boot$slot;
-is_slot_device=1;
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+# boot shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
@@ -28,25 +32,16 @@ patch_vbmeta_flag=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-## AnyKernel install
-split_boot;
-
 ## Selinux Permissive
 #patch_cmdline androidboot.selinux androidboot.selinux=permissive
 
 # migrate from /overlay to /overlay.d to enable SAR Magisk
-if [ -d $ramdisk/overlay ]; then
-  rm -rf $ramdisk/overlay;
-fi;
 
 # end ramdisk changes
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-flash_boot;
+write_boot; 
 ## end install
 
 sleep 3
